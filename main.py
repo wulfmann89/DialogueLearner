d124075febc0c79dad015bc1916263d28ce922e1
wulfmann89 <162912675+wulfmann89@users.noreply.github.com>
2025-10-03 17:01:33 +0000

    added files

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+
+# Launch Module for Corpus filtering
+
+# Launch Module for trait tagging
+
+# Launch Module for dialogue generation
+
+# Launch Module for progressive ML
+
+# CLI Flags
+
+# Menu 
+
